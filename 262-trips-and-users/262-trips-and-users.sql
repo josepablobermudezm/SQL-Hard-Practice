eefe6ef9a23a4d326acb591961ffde1906a9f426
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-11 16:27:04 -0600

    Time: 411 ms (97.68%), Space: 0B (100.00%) - LeetHub

---
+++ 262-trips-and-users/262-trips-and-users.sql
@@ -0,0 +1,64 @@
+# Write your MySQL query statement below
+
+
+with a as (
+select count(*) as total, t.request_at
+from Trips as t
+left join Users as u
+on u.users_id = t.client_id
+left join Users as u2
+on u2.users_id = t.driver_id
+where u2.banned <> 'Yes' 
+and u.banned <> 'Yes'
+and (t.status = 'cancelled_by_driver' or t.status = 'cancelled_by_client')
+and t.request_at between '2013-10-01' and '2013-10-03'
+group by request_at
+), b as (
+
+select count(*) as total, request_at
+from Trips as t
+left join Users as u
+on u.users_id = t.client_id
+left join Users as u2
+on u2.users_id = t.driver_id
+where u2.banned <> 'Yes' 
+and u.banned <> 'Yes'
+and t.request_at between '2013-10-01' and '2013-10-03'
+group by request_at
+),
+
+c as (
+select 
+    0 as total, 
+    t.request_at
+from Trips as t
+left join Users as u
+on u.users_id = t.client_id
+left join Users as u2
+on u2.users_id = t.driver_id
+where u2.banned <> 'Yes' 
+and u.banned <> 'Yes'
+and t.request_at between '2013-10-01' and '2013-10-03'
+and t.request_at not in (select request_at from a)
+group by request_at
+),
+
+d as (
+select 
+    a.request_at as Day, 
+    round((a.total / b.total), 2) as `Cancellation Rate`
+from a
+left join b
+on a.request_at = b.request_at
+union 
+select 
+    c.request_at, 
+    total
+from c
+)
+
+select * from d
+order by Day
+    
+
+
