a1100e4a06a19c4d1d97027b5f386a8a2086b93e
Jose Pablo Bermúdez <53883209+josepablobermudezm@users.noreply.github.com>
2022-07-11 16:29:38 -0600

    Time: 438 ms (90.98%), Space: 0B (100.00%) - LeetHub

--- 262-trips-and-users/262-trips-and-users.sql
+++ 262-trips-and-users/262-trips-and-users.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
 
+/*
 
+## first long approach
 with a as (
 select count(*) as total, t.request_at
 from Trips as t
@@ -60,5 +62,15 @@ from c
 select * from d
 order by Day
     
+    */
+    
+select 
+    t.Request_at Day,
+    ROUND((count(IF(t.status!='completed',TRUE,null))/count(*)),2) as 'Cancellation Rate'
+from Trips t 
+where t.Client_Id in (Select Users_Id from Users where Banned='No') 
+and t.Driver_Id in (Select Users_Id from Users where Banned='No')
+and t.Request_at between '2013-10-01' and '2013-10-03'
+group by t.Request_at;
 
 
